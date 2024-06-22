934ee8172810fa144ff928dae89f4af5b45f0f32
Jorge Avila <jorgelars18@gmail.com>
2024-06-22 18:46:28 -0400

    3

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import HomePageView, DetailPageView
+from .views import UpdatePageView, CreatePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(),name= "home"),
-    path("detail/<int:pk>",DetailPageView.as_view(), name = "detail")
+    path("detail/<int:pk>",DetailPageView.as_view(), name = "detail"),
+    path("create", CreatePageView.as_view(),name= "create"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+
+from django.views.generic.edit import UpdateView, CreateView
 from .models import Publicaciones
 
 # Create your views here.
@@ -12,3 +14,17 @@ class HomePageView(ListView):
 class DetailPageView(DetailView):
     template_name = "post_detail.html"
     model = Publicaciones
+
+# Para editar
+
+class UpdatePageView(UpdateView):
+    template_name = "post_update.html"
+    model = Publicaciones
+    fields = [
+        "titulo", "descrpcion"
+    ]
+
+class CreatePageView(CreateView):
+    template_name = "post_create.html"
+    model = Publicaciones
+    fields = ["titulo","descrpcion"]
